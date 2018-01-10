ba8ee884c2ba9683c7a102d24233e0af74f5d61b
carlos <carlos.r.montesinos@gmail.com>
2018-01-09 20:51:08 -0800

    Cleaned the code and now we are serving the page as expected.

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 import json
 import webapp2
 import model
+import string
 
-APP = webapp2.WSGIApplication([], debug=True)
+WEB_SITE = """<!doctype html> 
+<html> 
+  
+  <body>  
+    <h1 style="width: 100%; text-align: center;" >Josue's Stock Account</h1> 
+
+    <h1 style="width: 100%; text-align: center;" > APPLE </h1>
+    <h2 style="width: 100%; text-align: center;" > Quantity: {quatity} </h2>
+    <h2 style="width: 100%; text-align: center;" > Last Price: {last_price} </h2>
+    <h2 style="width: 100%; text-align: center;" > Current Value: {current_value} </h2> 
+
+  </body>
+</html>"""
+
+# Get data from DB
+
+# Look for tockens and replace
+UPDATED_WEB_SITE = string.replace(WEB_SITE, "{quatity}", "WE ROCK")
+UPDATED_WEB_SITE = string.replace(UPDATED_WEB_SITE, "{last_price}", "WE ROCK")
+UPDATED_WEB_SITE = string.replace(UPDATED_WEB_SITE, "{current_value}", "WE ROCK")
+
+
+# Serve website
+UPDATE_STRING = "UPDATING...!!!"
+
+class RestHandler(webapp2.RequestHandler):
+    
+    def get(self):
+     	# Serve the website
+    	self.response.write( UPDATED_WEB_SITE )
+
+APP = webapp2.WSGIApplication([
+    ('/', RestHandler),
+], debug=True)
